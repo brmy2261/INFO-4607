4285f8378dad1f4e0f6ce81352331434ec0d5265
haal5146-oss <haal5146@colorado.edu>
2026-03-30 11:29:47 -0600

    Add files via upload

---
+++ database.py
@@ -0,0 +1,39 @@
+# bridge between fastapi and postgres
+# how to talk to postgres
+
+import os
+from urllib.parse import quote_plus
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+host = os.getenv("POSTGRES_HOST", "localhost")
+port = os.getenv("POSTGRES_PORT", "5432")
+db = os.getenv("POSTGRES_DB")
+user = os.getenv("POSTGRES_USER")
+pw = os.getenv("POSTGRES_PASSWORD")
+
+if not all([db, user, pw]):
+    raise RuntimeError("missing pw, db, user")
+
+database_url = (
+    
+    f"postgresql+psycopg2://{user}:{quote_plus(pw)}@{host}:{port}/{quote_plus(db)}"
+
+)
+
+engine = create_engine(
+    database_url, 
+    pool_pre_ping=True,
+    connect_args={"options": "-csearch_path=third_iteration"}
+) #this is what fastapi talks to
+
+sessionlocal = sessionmaker(autocommit=False, autoflash=False, bind=engine)
+# each api request gets its own database session
+
+def test_connection():
+    with engine.connect() as conn:
+        return conn.execute(text("select 1")).scalar_one()
